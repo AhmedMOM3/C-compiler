35f0199c155500c8770f66513a6f604da28ccd72
Ahmed Mohamed Abdelalem Omar <142239583+AhmedMOM3@users.noreply.github.com>
2024-11-08 18:26:11 +0200

    Add files via upload

---
+++ Lexical analyzer/input.c
@@ -0,0 +1,14 @@
+// Simple C program to display "Hello World"
+// Header file for input output functions
+#include <stdio.h>
+// #include <stdio.h>
+// main function -
+// where the execution of program begins
+int main()
+{
+    // prints hello world
+    int x = 15;
+    char x[] = "how are you man";
+    printf("Hello World"); // hello world comment
+    return 0;
+}
